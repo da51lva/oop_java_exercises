163cc1a37e61cfde3904ec29c8f69fcf59695bc1
Matt DaSilva <matt_das@hotmail.co.uk>
2022-04-03 18:27:01 +0100

    Implement Cat Interface

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -2,4 +2,19 @@ package com.techreturners.cats;
 
 interface Cat {
 
+    String eat();
+
+    void goToSleep();
+
+    void wakeUp();
+
+    void run();
+
+    boolean isAsleep();
+
+    int getAverageHeight();
+
+    String getSetting();
+
+
 }
